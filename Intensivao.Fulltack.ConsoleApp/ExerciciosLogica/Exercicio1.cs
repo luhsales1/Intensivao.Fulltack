f4d5139cfca5e41555495dc5c53d658951d0a666
Luciana Sales <lulu.isis@hotmail.com>
2023-02-07 21:52:49 -0300

    Referente ao exercício 1

---
+++ Intensivao.Fulltack.ConsoleApp/ExerciciosLogica/Exercicio1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Exercicio1
+    {
+           public static void Calcular()
+           {
+            Console.WriteLine("1º Crie um vetor com nomes de todos os integrantes do intensivão exiba todos os nomes, \r\n  e também faça com que acrescente os sobrenomes para cada integrante e exiba novamente com os nomes completos.");
+            Console.WriteLine();
+           }
+    }
+
